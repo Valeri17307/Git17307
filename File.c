9627035b4379602010a1be35905f5a9af5cc4c01
Valeri17307 <valerinikolov2866@gmail.com>
2021-11-24 09:41:36 +0200

    Rp

--- File.c
+++ File.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int xb, yb, xa, ya, A, B;
+	int xb, yb, xa, yc, A, B;
 	printf("Xb: ");
 	scanf("%d", &xb);
         printf("Ya: ");
 	scanf("%d", &ya);
         printf("Xa: ");
 	scanf("%d", &xa);
-        printf("Yb: ");
-	scanf("%d", &yb);
+        printf("Yc: ");
+	scanf("%d", &yc);
 	A = xa + ya;
         B = xb + yb;
         printf("A = xa + ya = %d\n", A);
